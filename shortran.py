707551b40ca21d45d0a5439410406891d61e7795
mmubarak0 <66879926+mmubarak0@users.noreply.github.com>
2020-09-13 09:13:15 +0200

    Update shortran.py

--- shortran.py
+++ shortran.py
@@ -1,17 +1,19 @@
 import os
 
-print("""
-[0]set shortran directory 
-[1]create new virtual env
+print(""" 
+( select your language ) --> Python
+[0]Change shortran directory 
+[1]Create new virtual env
 [2]your projects (you can activate your env from here)
 [3]install application or package 
-[4]create basic program structure for:
-[5]search for error in stack, github, ...
+[4]
+[5]
 [6]command list shortcut for django
 [7]create your own command list shortcut for 
 [8]deploy your application on internet
-[9]create fps game
-[10]download app from apk pure
+[9]
+[10]show list of directory and sub directory
 [11]suggest more shortcuts
 [12]share this repository
 """)
+output = input(" enter number [10] : ")
