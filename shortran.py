92ec72b84075be560a15373dbc8d89189c9617ed
mmubarak0 <66879926+mmubarak0@users.noreply.github.com>
2020-09-13 09:05:08 +0200

    Add files via upload
    this is under develop project it aims to make an simple shortcuts for all command needed in the go 
    --> this year goals covering the following projects command
    </ Django />
    </ Virtual Environment />
    </ Windows Shell />
    
    optional { it can be removed in future release }
    << Adding some shortcuts for downloads >>

---
+++ shortran.py
@@ -0,0 +1,17 @@
+import os
+
+print("""
+[0]set shortran directory 
+[1]create new virtual env
+[2]your projects (you can activate your env from here)
+[3]install application or package 
+[4]create basic program structure for:
+[5]search for error in stack, github, ...
+[6]command list shortcut for django
+[7]create your own command list shortcut for 
+[8]deploy your application on internet
+[9]create fps game
+[10]download app from apk pure
+[11]suggest more shortcuts
+[12]share this repository
+""")
